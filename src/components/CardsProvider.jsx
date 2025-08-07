783e89a1db392d25c00c132202758e9881f9c4ef
Movash <gercakbog@gmail.com>
2025-08-07 21:56:23 +0300

    localstorage

--- src/components/CardsProvider.jsx
+++ src/components/CardsProvider.jsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CardsContext } from "./CardsContext";
 
 export function CardsProvider({ children }) {
-  const [cards, setCards] = useState([]);
+  const [cards, setCards] = useState(() => {
+    const saved = localStorage.getItem("cards");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("cards", JSON.stringify(cards));
+  }, [cards]);
 
   return (
     <CardsContext.Provider value={{ cards, setCards }}>
