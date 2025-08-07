db6be21b7665cb104b9104a3e6099e41192f8fef
Movash <gercakbog@gmail.com>
2025-08-07 21:10:24 +0300

    basename

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { CardsProvider } from "./components/CardsProvider";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CardsProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/counter">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
